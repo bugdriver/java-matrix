7499e04370874b314aa11c1abbfff1c3379d7d49
rajat <rajat4113@gmail.com>
2020-10-22 11:36:41 +0530

    added tests and build scripts

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-rm -rf out/*
+rm -rf out
+mkdir out
 javac -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar -d out $(find . -name "*.java")
--- src/com/step/math/MatrixTest.java
+++ src/com/step/math/MatrixTest.java
@@ -2,19 +2,52 @@ package com.step.math;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MatrixTest {
+  private Matrix matrix1;
+  private Matrix matrix2;
+
+  private void initMatrices(int[][] matrix1, int[][] matrix2) {
+    this.matrix1 = new Matrix(matrix1, 3, 3);
+    this.matrix2 = new Matrix(matrix2, 3, 3);
+  }
+
+  @Before
+  public void initMatrices() {
+    int[][] m1 = { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
+    int[][] m2 = { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
+    this.initMatrices(m1, m2);
+  }
 
   @Test
   public void shouldAddTwoMatrices() {
-    int[][] m1 = { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
-    int[][] m2 = { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
     int[][] expected = { { 2, 2, 2 }, { 4, 4, 4 }, { 6, 6, 6 } };
-    Matrix matrix1 = new Matrix(m1, 3, 3);
-    Matrix matrix2 = new Matrix(m2, 3, 3);
     Matrix expectedMatrix = new Matrix(expected, 3, 3);
-    Matrix actual = matrix1.add(matrix2);
+    Matrix actual = this.matrix1.add(this.matrix2);
     assertEquals("should add two Matrices", expectedMatrix, actual);
+  }
+
+  @Test
+  public void shouldSubtractTwoMatrices() {
+    int[][] expected = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+    Matrix expectedMatrix = new Matrix(expected, 3, 3);
+    Matrix actual = this.matrix1.subtract(this.matrix2);
+    assertEquals("should subtract two Matrices", expectedMatrix, actual);
+  }
+
+  @Test
+  public void shouldMultiplyTwoMatrices() {
+    int[][] expected = { { 6, 6, 6 }, { 12, 12, 12 }, { 18, 18, 18 } };
+    Matrix expectedMatrix = new Matrix(expected, 3, 3);
+    Matrix actual = this.matrix1.multiply(this.matrix2);
+    assertEquals("should multiply two Matrices", expectedMatrix, actual);
+  }
+
+  @Test
+  public void shouldGiveDeterminantOfMatrix() {
+    int actual = this.matrix1.determinant();
+    assertEquals("should give determinant of Matrix", 0, actual);
   }
 }
